99c7654ebe27f62f63624d3e762f9aa15a321f02
Laxman <mohitrawatsingh440@gmail.com>
2026-09-03 19:37:43 +0530

    feat: add JavaScript basics

---
+++ 01_Basics/01-variables.js
@@ -0,0 +1,15 @@
+// Variables in JavaScript
+
+const accountId = 1234;
+let accountEmail = "mohit12@gmail.com";
+var accountPassword = "121212";
+accountCity = "tharali";
+
+// Chaangin Values
+// not allowed -> accountId = 2;
+accountEmail = "mohitrawat@gmail.com";
+accountPassword = "23456";
+accountCity = "tungeshwar";
+
+// console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
